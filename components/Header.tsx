c3872698e480cc0b91fc710c14e1e441f62044d9
Finleyoconnell <122997912+Finleyoconnell@users.noreply.github.com>
2024-01-30 12:47:19 -0600

    auth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from 'next-auth/react';
+
 
 const Header: React.FC = () => {
   const router = useRouter();
@@ -72,11 +73,87 @@ let right = null
     );
   }
 
+  if (!session) {
+    right = (
+      <div className="right">
+        <Link href="/api/auth/signin">
+          <a data-active={isActive('/signup')}>Log in</a>
+        </Link>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+        `}</style>
+      </div>
+    );
+  }
+
+  if (session) {
+    
+    right = (
+      <div className="right">
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <button onClick={() => signOut()}>
+          <a>Log out</a>
+        </button>
+        <style jsx>{`
+          a {
+            text-decoration: none;
+            color: var(--geist-foreground);
+            display: inline-block;
+          }
+
+          p {
+            display: inline-block;
+            font-size: 13px;
+            padding-right: 1rem;
+          }
+
+          a + a {
+            margin-left: 1rem;
+          }
+
+          .right {
+            margin-left: auto;
+          }
+
+          .right a {
+            border: 1px solid var(--geist-foreground);
+            padding: 0.5rem 1rem;
+            border-radius: 3px;
+          }
+
+          button {
+            border: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
 
   return (
     <nav>
       {left}
       {center}
+      {right}
       <style jsx>{`
         nav {
           display: flex;
